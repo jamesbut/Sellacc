5d47b16cd53a8c079f287435694b0cc1d3f6601f
James Butterworth <jjbsports16@hotmail.co.uk>
2020-07-12 12:11:05 +0100

    Reads and parses transactions csv file

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,34 @@ use csv::Reader;
 use std::error::Error;
 use std::fs::File;
 
-fn read_transactions_csv(file_name: &str) -> Result<(), Box<dyn Error>> {
+fn process_transactions_reader(transactions_reader: &mut csv::Reader<File>) -> Vec<Vec<String>> {
 
-    println!("Reading transactions from file: {}", file_name);
+    for result in transactions_reader.records() {
 
-    let mut reader = Reader::from_path(file_name)?;
-    for result in reader.records() {
-
-        let record = result?;
+        let record = result.unwrap();
         println!("{:?}", record);
 
     }
 
-    Ok(())
+    let data: Vec<Vec<String>> = Vec::new();
+
+    data
+
+}
+
+//fn read_transactions_csv(file_name: &str) -> Result<(), Box<dyn Error>> {
+fn read_transactions_csv(file_name: &str) {
+
+    println!("Reading transactions from file: {}", file_name);
+
+    let mut reader = Reader::from_path(file_name).unwrap();
+    let data = process_transactions_reader(&mut reader);
 
 }
 
 fn main() {
 
-    let current_dir = std::env::current_exe().unwrap().to_str().unwrap().to_owned();
-    let file_name = current_dir + "/../../../resources/transactions.csv";
-    let mut file = File::open("foo.txt");
-    //read_transactions_csv(&file_name);
+    let file_name = "resources/transactions.csv";
+    read_transactions_csv(&file_name);
 
 }
